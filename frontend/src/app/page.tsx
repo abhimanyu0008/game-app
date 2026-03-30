b87437c54f8e83ff2481b12e600a8083d780cbef
Abhimanyu Kumar <studywala7646@gmail.com>
2026-03-31 01:21:17 +0530

    your message

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation of testing
+            Documentation of the java 
           </a>
         </div>
       </main>
